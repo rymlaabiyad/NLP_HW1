4ccd8b4b654bbcf25ff703fb0ec9e5fc67853a25
rymlaabiyad <rym.laab@gmail.com>
2019-02-19 14:19:05 +0100

    Removing an m at the end of the file

--- skipGram.py
+++ skipGram.py
@@ -158,7 +158,7 @@ if __name__ == '__main__':
     else:
         pairs = loadPairs(opts.text)
 
-        sg = mSkipGram.load(opts.model)
+        sg = SkipGram.load(opts.model)
         for a,b,_ in pairs:
             print(sg.similarity(a,b))
 
